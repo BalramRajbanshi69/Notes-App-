2ff03620fcdd3313db235d4d4e2254b4c81832b4
BALRAM RAJBANSHI <95425462+BalramRajbanshi69@users.noreply.github.com>
2024-04-29 13:43:35 +0545

    Add files via upload

---
+++ Notes App JavaScript Project/notes.js
@@ -0,0 +1,46 @@
+const btn = document.querySelector(".btn");
+const notesContainer = document.querySelector(".notes-container")
+let notes = document.querySelectorAll(".input-box")
+
+
+function showNotes(){
+    notesContainer.innerHTML= localStorage.getItem("notes");
+}
+showNotes();
+
+
+function storageUpdate(){
+    localStorage.setItem("notes", notesContainer.innerHTML)
+}
+
+
+btn.addEventListener("click",()=>{
+    let inputBox= document.createElement("p")
+    let img = document.createElement("img");
+    inputBox.className= "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "./images/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+})
+
+
+notesContainer.addEventListener("click",(e)=>{
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+        storageUpdate();
+    }else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll("input-box");
+        notes.forEach((note)=>{
+            note.onkeyup = function(){
+                storageUpdate();
+            }
+        })
+    }
+}) 
+
+document.addEventListener("keydown", (event)=>{
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+})
